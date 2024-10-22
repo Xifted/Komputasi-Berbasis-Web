5180ceeaa4d2d040c897f3c1d611af0970507a1b
Xifted <oneisfirst01@gmail.com>
2024-10-22 12:31:53 +0700

    Update Minggu 7

--- Minggu 7/muhammad_rafi_irfan.sql
+++ Minggu 7/muhammad_rafi_irfan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 14, 2024 at 01:46 PM
+-- Generation Time: Oct 22, 2024 at 05:31 AM
 -- Server version: 9.0.1
 -- PHP Version: 8.3.12
 
@@ -41,9 +41,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `hobbies`, `gender`, `birthDate`) VALUES
-(2, 'Muhammad Rafi Irfan', 'adasdsa@fasdas.com', 'membaca, menyanyi, lari', 'male', '2000-01-01'),
-(3, 'Gabriel', 'adasdsa@fasdas.com', 'membaca, menyanyi', 'male', '2000-01-01'),
-(4, 'Gabriel', 'adasdsa@fasdas.com', 'membaca, menyanyi', 'male', '2000-01-01');
+(44, 'Ignatius Pruittt', 'xasy@mailinator.comm', 'menyanyi', 'male', '1990-07-18'),
+(45, 'Cailin Velasquez', 'megobiw@mailinator.com', 'menyanyi, lari', 'male', '1972-06-08'),
+(46, 'Drake Sharp', 'mibuce@mailinator.com', 'membaca, menyanyi, lari', 'female', '1975-05-02'),
+(47, 'Idona Perez', 'deduzy@mailinator.com', 'lari', 'female', '2006-12-12'),
+(48, 'Silas Henderson', 'bydorisew@mailinator.com', 'membaca, menyanyi', 'male', '1994-05-07'),
+(49, 'Burton Bender', 'qucuwagy@mailinator.com', 'membaca, menyanyi, lari', 'female', '2022-04-30'),
+(50, 'Lucy Duncan', 'jiguxudiqy@mailinator.com', 'lari', 'male', '2012-03-29'),
+(51, 'Oleg Schultz', 'zasosyr@mailinator.com', 'menyanyi', 'female', '2005-05-25'),
+(53, 'Keiko Deleon', 'jusagidyf@mailinator.com', 'menyanyi', 'male', '1992-02-25'),
+(54, 'Kaseem Barker', 'zybic@mailinator.com', 'membaca, lari', 'male', '2000-02-24');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +70,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
